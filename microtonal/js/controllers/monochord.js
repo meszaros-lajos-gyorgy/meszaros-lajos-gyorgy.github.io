e5bfcba1b9fd4bb7cfc931894cf57b9c4ccfbf42
Lajos Meszaros <lajos.meszaros@performgroup.com>
2015-08-27 17:53:40 +0200

    found new format for the ratios and moved some codes to their new places. +some code abstraction

--- microtonal/js/controllers/monochord.js
+++ microtonal/js/controllers/monochord.js
@@ -1,51 +1,61 @@
 define(['app', 'components/menu', 'components/string-to-number'], function(app){
 	'use strict';
 	
+	// https://en.wikipedia.org/wiki/List_of_pitch_intervals
+	// https://en.wikipedia.org/wiki/Equal_temperament
+	
 	var lastStringId = 0;
 	var lastSetId = 0;
+	
 	var oscillators = {};
 	var stringGains = {};
 	var setGains = {};
 	
+	function stopAll(){
+		// todo
+		/*
+		Object.keys(gains).forEach(function(key){
+			var gain = gains[key];
+			gain.gain.value = 0;
+			gain.disconnect(mainGain);
+			
+			var oscillator = oscillators[key];
+			oscillator.stop();
+			oscillator.disconnect(gain);
+		});
+		*/
+		oscillators = {};
+		stringGains = {};
+		setGains = {};
+	}
+	
+	/*
+	function addFreq(ctx, freq){
+		var o = ctx.createOscillator();
+		o.type = 'sine';
+		o.frequency.value = freq;
+		o.start();
+		return o;
+	}
+	
+	var ctx;
+	try{
+		ctx = new (window.AudioContext || window.webkitAudioContext)();
+	}catch(e){
+		alert('Web Audio API is not supported by this browser');
+	}
+	
+	var mainGain = ctx.createGain();
+	mainGain.connect(ctx.destination);
+	mainGain.gain.value = 1;
+	*/
+	
+	// ---------------------
+	
 	app.controller('MonochordCtrl', ['$scope', '$http', '$stateParams', '$state', '$rootScope', function($scope, $http, $stateParams, $state, $rootScope){
 		$scope.baseFrequency = 100;
-		$scope.sets = [{
-			id : 1,
-			normalize : {
-				type : 'manual',
-				target : 2
-			},
-			volume : 100,
-			strings : [{
-				id : 1,
-				volume : 0.3,
-				multiplier : 4
-			}, {
-				id : 2,
-				volume : 0.3,
-				multiplier : 5
-			}, {
-				id : 3,
-				volume : 0.3,
-				multiplier : 6
-			}]
-		}, {
-			id : 2,
-			normalize : {
-				type : 'manual',
-				target : 4
-			},
-			volume : 100,
-			strings : [{
-				id : 4,
-				volume : 0.3,
-				multiplier : 21
-			}, {
-				id : 5,
-				volume : 0.3,
-				multiplier : 25
-			}]
-		}];
+		$scope.sets = [];
+		$scope.presets = {};
 		
 		$scope.addSet = function(){
 			$scope.sets.push({
@@ -90,6 +100,50 @@ define(['app', 'components/menu', 'components/string-to-number'], function(app){
 			});
 		};
 		
+		function _import(){
+			// todo
+			return [{
+				id : 1,
+				normalize : {
+					type : 'manual',
+					target : 2
+				},
+				volume : 100,
+				strings : [{
+					id : 1,
+					volume : 0.3,
+					multiplier : 4
+				}, {
+					id : 2,
+					volume : 0.3,
+					multiplier : 5
+				}, {
+					id : 3,
+					volume : 0.3,
+					multiplier : 6
+				}]
+			}, {
+				id : 2,
+				normalize : {
+					type : 'manual',
+					target : 4
+				},
+				volume : 100,
+				strings : [{
+					id : 4,
+					volume : 0.3,
+					multiplier : 21
+				}, {
+					id : 5,
+					volume : 0.3,
+					multiplier : 25
+				}]
+			}];
+		};
+		function _export(){
+			return ''; // todo
+		}
+		
 		function calculateFrequency(stringId){
 			// this did not include the normalize procedure
 			// return $scope.baseFrequency * string.multiplier;
@@ -112,52 +166,113 @@ define(['app', 'components/menu', 'components/string-to-number'], function(app){
 				});
 			});
 		}, true);
+		
+		function diffSets(newValue, oldValue){
+			var diff = {
+				changed : [],
+				added : [],
+				removed : []
+			};
+			
+			// todo
+			/*
+			newValue.forEach(function(value){
+				(
+					oldValue.some(function(oldValue){
+						return (value.id === oldValue.id);
+					})
+					? changed
+					: added
+				).push(value);
+				ratios.push(value.multiplier);
+			});
+			
+			oldValue.forEach(function(value){
+				if(!changed.some(function(chg){
+					return (chg.id === value.id);
+				}) && !added.some(function(add){
+					return (add.id === value.id);
+				})){
+					removed.push(value);
+				}
+			});
+			*/
+			
+			return diff;
+		}
+		
+		$scope.$watch('sets', function(newValue, oldValue){
+			if(JSON.stringify(newValue) === '[]' && JSON.stringify(oldValue) === '[]'){
+				return ;
+			}
+			
+			var diff = diffSets(newValue, oldValue);
+			
+			// todo
+			/*
+			var ratios = [];
+			
+			if(loadedFromURL === null){
+				stopAll();
+			}
+			
+			changed.forEach(function(string){
+				oscillators[string.id].frequency.value = $scope.baseFrequency * string.multiplier;
+				gains[string.id].gain.value = string.volume / 100;
+			});
+			
+			added.forEach(function(string){
+				var o = addFreq(ctx, $scope.baseFrequency * string.multiplier);
+				var g = ctx.createGain();
+				g.connect(mainGain);
+				o.connect(g);
+				g.gain.value = string.volume / 100;
+				
+				oscillators[string.id] = o;
+				gains[string.id] = g;
+			});
+			
+			removed.forEach(function(string){
+				gains[string.id].disconnect(mainGain);
+				oscillators[string.id].stop();
+				delete gains[string.id];
+				delete oscillators[string.id];
+			});
+			
+			ratios = ratios.sort(function(a, b){
+				return a - b;
+			});
+			if($scope.normalize){
+				var normalizedBaseFreq = $scope.baseFrequency / ratios[0];
+				var i = 0;
+				Object.keys(oscillators).forEach(function(stringId){
+					oscillators[stringId].frequency.value = normalizedBaseFreq * ratios[i];
+					i++;
+				})
+			}
+			
+			var url = 'basefreq/' + $scope.baseFrequency + '/ratios/' + decodeURIComponent(encodeURIComponent(ratios));
+			$state.go('monochord', {route : url}, {notify : false});
+			
+			if(loadedFromURL !== false){
+				loadedFromURL = false;
+			}
+			*/
+		});
+		
+		// this was loading unnecessarily many times in the previous version:
+		$http.get('presets.json').success(function(data){
+			$scope.presets = data;
+		});
+		
+		// temporary code:
+		$scope.sets = _import();
 	}]);
 	
 	/*
-	function addFreq(ctx, freq){
-		var o = ctx.createOscillator();
-		o.type = 'sine';
-		o.frequency.value = freq;
-		o.start();
-		return o;
-	}
-	
-	function stopAll(){
-		Object.keys(gains).forEach(function(key){
-			var gain = gains[key];
-			gain.gain.value = 0;
-			gain.disconnect(mainGain);
-			
-			var oscillator = oscillators[key];
-			oscillator.stop();
-			oscillator.disconnect(gain);
-		});
-		gains = [];
-		oscillators = [];
-	}
-	
-	var ctx;
-	try{
-		ctx = new (window.AudioContext || window.webkitAudioContext)();
-	}catch(e){
-		alert('Web Audio API is not supported by this browser');
-	}
-	
-	var mainGain = ctx.createGain();
-	mainGain.connect(ctx.destination);
-	mainGain.gain.value = 1;
-	
 	app.controller('MonochordCtrl', ['$scope', '$http', '$stateParams', '$state', '$rootScope', function($scope, $http, $stateParams, $state, $rootScope){
-		// https://en.wikipedia.org/wiki/List_of_pitch_intervals
-		// https://en.wikipedia.org/wiki/Equal_temperament
-		
 		var BASE_HREF = '/monochord/';
 		var loadedFromURL = false; // change type of $scope.strings: false = by UI, null = by URL, true = should ignore, because we changed URL by code
-		
-		$scope.ratios = [];
-		$scope.strings = [];
-		$scope.normalize = false;
 		
 		// ------------------
 		
@@ -201,90 +316,7 @@ define(['app', 'components/menu', 'components/string-to-number'], function(app){
 			}
 		});
 		
-		$scope.$watch('strings', function(newValue, oldValue){
-			if(JSON.stringify(newValue) === '[]' && JSON.stringify(oldValue) === '[]'){
-				return ;
-			}
-			
-			var changed = [];
-			var removed = [];
-			var added = [];
-			var ratios = [];
-			
-			if(loadedFromURL === null){
-				stopAll();
-			}
-			
-			newValue.forEach(function(value){
-				(
-					oldValue.some(function(oldValue){
-						return (value.id === oldValue.id);
-					})
-					? changed
-					: added
-				).push(value);
-				ratios.push(value.multiplier);
-			});
-			
-			oldValue.forEach(function(value){
-				if(!changed.some(function(chg){
-					return (chg.id === value.id);
-				}) && !added.some(function(add){
-					return (add.id === value.id);
-				})){
-					removed.push(value);
-				}
-			});
-			
-			changed.forEach(function(string){
-				oscillators[string.id].frequency.value = $scope.baseFrequency * string.multiplier;
-				gains[string.id].gain.value = string.volume / 100;
-			});
-			
-			added.forEach(function(string){
-				var o = addFreq(ctx, $scope.baseFrequency * string.multiplier);
-				var g = ctx.createGain();
-				g.connect(mainGain);
-				o.connect(g);
-				g.gain.value = string.volume / 100;
-				
-				oscillators[string.id] = o;
-				gains[string.id] = g;
-			});
-			
-			removed.forEach(function(string){
-				gains[string.id].disconnect(mainGain);
-				oscillators[string.id].stop();
-				delete gains[string.id];
-				delete oscillators[string.id];
-			});
-			
-			ratios = ratios.sort(function(a, b){
-				return a - b;
-			});
-			if($scope.normalize){
-				var normalizedBaseFreq = $scope.baseFrequency / ratios[0];
-				var i = 0;
-				Object.keys(oscillators).forEach(function(stringId){
-					oscillators[stringId].frequency.value = normalizedBaseFreq * ratios[i];
-					i++;
-				})
-			}
-			
-			var url = 'basefreq/' + $scope.baseFrequency + '/ratios/' + decodeURIComponent(encodeURIComponent(ratios));
-			$state.go('monochord', {route : url}, {notify : false});
-			
-			if(loadedFromURL !== false){
-				loadedFromURL = false;
-			}
-		}, true);
-		
 		// ------------------
-		
-		// this loads unnecessarily many times
-		$http.get('ratios.json').success(function(data){
-			$scope.ratios = data;
-		});
 		
 		$rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
 			loadedFromURL = true;
