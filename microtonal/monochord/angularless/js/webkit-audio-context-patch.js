1a21cd06f33b6a74720e301f61e2da4141b47b6f
Lajos Meszaros <lajos.meszaros@performgroup.com>
2015-09-11 17:21:56 +0200

    OscillatorNode.start's and .stop's first parameter is now optional

--- microtonal/monochord/angularless/js/webkit-audio-context-patch.js
+++ microtonal/monochord/angularless/js/webkit-audio-context-patch.js
@@ -11,14 +11,23 @@
 			a.prototype.createGain = a.prototype.createGainNode;
 		}
 		
-		// todo: make first parameter optional (for firefox <30)
 		if(!OscillatorNode.prototype.hasOwnProperty('start')){
 			OscillatorNode.prototype.start = OscillatorNode.prototype.noteOn;
 		}
-		// todo: make first parameter optional (for firefox <30)
+		// make the first parameter optional for firefox <30
+		var oldStart = OscillatorNode.prototype.start;
+		OscillatorNode.prototype.start = function(t){
+			oldStart(t || 0);
+		};
+		
 		if(!OscillatorNode.prototype.hasOwnProperty('stop')){
 			OscillatorNode.prototype.stop = OscillatorNode.prototype.noteOff;
 		}
+		// make the first parameter optional for firefox <30
+		var oldStop = OscillatorNode.prototype.stop;
+		OscillatorNode.prototype.stop = function(t){
+			oldStop(t || 0);
+		};
 		
 		Object.defineProperty(OscillatorNode.prototype, 'type', {
 			get : function(){
