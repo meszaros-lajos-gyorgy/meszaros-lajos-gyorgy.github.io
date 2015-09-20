41edc88dd040f5421c6e317ea01558df643859de
Louis <m_lajos@hotmail.com>
2015-09-20 12:38:36 +0200

    added mutation observer to auto init new dom elements

--- microtonal/monochord/angularless/js/binder.js
+++ microtonal/monochord/angularless/js/binder.js
@@ -3,26 +3,61 @@
 	
 	var $scope;
 	
-	function init(){
-		var onChange = function(e){
-			if(e.target.hasAttribute('data-model')){
-				var model = e.target.getAttribute('data-model');
-				var value = e.target.value;
-				if(!$scope.hasOwnProperty(model)){
-					$scope.register(model);
-				}
-				$scope[model] = value;
+	function isInput(element){
+		return (['input', 'textarea', 'select'].indexOf(element.nodeName.toLowerCase()) !== -1);
+	}
+	
+	function onChangeHandler(e){
+		if(e.target.hasAttribute('data-model')){
+			var model = e.target.getAttribute('data-model');
+			var value = e.target.value;
+			if(!$scope.hasOwnProperty(model)){
+				$scope.register(model);
 			}
-		};
+			$scope[model] = value;
+		}
+	};
+	
+	function initValues(view){
+		var elements = [].slice.call(view.querySelectorAll('[data-model]'));
+		if(view.hasAttribute('data-model')){
+			elements.unshift(view);
+		}
+		elements.forEach(function(element){
+			var model = element.getAttribute('data-model');
+			var value = '';
+			if($scope.hasOwnProperty(model)){
+				value = $scope[model];	
+			}
+			element[isInput(element) ? 'value' : 'textContent'] = value;
+		});
+	}
+	
+	function initDomEvents(){
+		document.body.addEventListener('change', onChangeHandler, true);
+		document.body.addEventListener('input', onChangeHandler, true);
 		
+		new MutationObserver(function(data){
+			[].slice.call(data[0].addedNodes).filter(function(){
+				return (data[0].addedNodes[0].nodeName.toLowerCase() !== '#text');
+			}).forEach(function(element){
+				initValues(element);
+			});
+		}).observe(document.body, {
+			childList : true,
+			attributes : true,
+			characterData : false,
+			subtree: true
+		});
+	}
+	
+	function init(){
 		if(document.readyState === 'interactive' || document.readyState === 'complete'){
-			document.body.addEventListener('change', onChange, true);
-			document.body.addEventListener('input', onChange, true);
+			initDomEvents();
 		}else{
 			document.addEventListener('readystatechange', function(){
 				if(document.readyState === 'interactive'){
-					document.body.addEventListener('change', onChange, true);
-					document.body.addEventListener('input', onChange, true);
+					initDomEvents();
 				}
 			});
 		}
@@ -30,27 +65,8 @@
 		$scope.$watch('__changed', function(e){
 			var model = e.property;
 			[].slice.call(document.querySelectorAll('[data-model="' + model + '"]')).forEach(function(element){
-				if(['input', 'textarea', 'select'].indexOf(element.nodeName.toLowerCase()) !== -1){
-					element.value = e.newValue;
-				}else{
-					element.textContent = e.newValue;
-				}
+				element[isInput(element) ? 'value' : 'textContent'] = e.newValue;
 			});
-		});
-	}
-	
-	function initValues(view){
-		[].slice.call(view.querySelectorAll('[data-model]')).forEach(function(element){
-			var model = element.getAttribute('data-model');
-			var value = '';
-			if($scope.hasOwnProperty(model)){
-				value = $scope[model];	
-			}
-			if(['input', 'textarea', 'select'].indexOf(element.nodeName.toLowerCase()) !== -1){
-				element.value = value;
-			}else{
-				element.textContent = value;
-			}
 		});
 	}
 	
@@ -61,7 +77,6 @@
 			}
 			$scope = newScope;
 			init();
-		},
-		initValues : initValues
+		}
 	};
 })();
--- microtonal/monochord/angularless/js/script.js
+++ microtonal/monochord/angularless/js/script.js
@@ -27,7 +27,6 @@
 	
 	Binder.bindScope(DataModel.$scope);
 	var ui = UI.get(DataModel.$scope);
-	Binder.initValues(ui);
 	
 	document.addEventListener('readystatechange', function(){
 		if(document.readyState === 'complete'){
@@ -37,18 +36,3 @@
 })();
 
 // var rawImportData = '[{"id":3,"normalize":{"type":"off","subject":0,"target":0},"volume":100,"strings":[{"id":6,"multiplier":4,"volume":"25"},{"id":7,"multiplier":5,"volume":"50"},{"id":8,"multiplier":"6","volume":"50"}]},{"id":5,"normalize":{"type":"manual","subject":9,"target":7},"volume":100,"strings":[{"id":9,"multiplier":21,"volume":"0"},{"id":10,"multiplier":25,"volume":"50"}]}]';
-
-document.addEventListener('readystatechange', function(){
-if(document.readyState === 'complete'){
-
-new MutationObserver(function(){
-	// we have mutations here!
-}).observe(document.body, {
-	childList : true,
-	attributes : true,
-	characterData : false,
-	subtree: true
-});
-
-}
-});
