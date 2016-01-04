44ed768f143734e67bf91a207e6d9b851dbc2285
Louis <m_lajos@hotmail.com>
2016-01-04 22:15:23 +0100

    Created a pure JS demo app to see, where should I go with the UI

--- microtonal/monochord/angularless/js/script.js
+++ microtonal/monochord/angularless/js/script.js
@@ -11,7 +11,7 @@
 	modules.AudioModel.addString(1, 1, {
 		volume : 1,
 		type : 'sine',
-		frequency : 220
+		frequency : 440
 	});
 	modules.AudioModel.addString(2, 1, {
 		volume : 1,
@@ -20,4 +20,96 @@
 	});
 	modules.AudioModel.setMainVolume(0.1);
 	modules.AudioModel.updateReal();
+	
+	// ----------
+	
+	var createElement = function(tagName, attributes, children){
+		var element = document.createElement(tagName);
+		
+		for(var name in attributes){
+			var value = attributes[name];
+			switch(name){
+				case 'class' :
+					element.className = value;
+					break;
+				case 'html' :
+					element.innerHTML = value;
+					break;
+				case 'text' :
+					element.textContent = value;
+					break;
+				case 'data' :
+					for(var attr in value){
+						element.setAttribute('data-' + attr, value[attr]);
+					}
+					break;
+				case 'on' :
+					for(var event in value){
+						element.addEventListener(event, value[event]);
+					}
+					break;
+				default :
+					element.setAttribute(name, value + '');
+			}
+		}
+		
+		if(children.push){
+			for(var i = 0; i < children.length; i++){
+				element.appendChild(children);
+			}
+		}
+		
+		return element;
+	};
+	
+	var onReady = function(handler){
+		var isValidReadyState = function(){
+			return (document.readyState === 'interactive' || document.readyState === 'complete');
+		};
+		
+		if(isValidReadyState()){
+			handler();
+		}else{
+			var rsHandler = function(){
+				document.removeEventListener('readystatechange', rsHandler);
+				
+				if(isValidReadyState()){
+					handler();
+				}
+			};
+			document.addEventListener('readystatechange', rsHandler);
+		}
+	};
+	
+	// ----------
+	
+	var mainVolume = 10;
+	
+	var getMainVolume = function(){
+		return mainVolume;
+	};
+	var setMainVolume = function(newValue){
+		if(newValue === mainVolume){
+			return;
+		}
+		
+		mainVolume = newValue;
+		
+		modules.AudioModel.setMainVolume(mainVolume / 100);
+		modules.AudioModel.updateReal();
+	};
+	
+	onready(function(){
+		document.body.appendChild(createElement('input', {
+			type : 'range',
+			min : 0,
+			max : 100,
+			value : getMainVolume(),
+			on : {
+				input : function(){
+					setMainVolume(this.value);
+				}
+			}
+		}));
+	});
 })(window.modules);
