a03e7f4639385e2f99a57f82d495f51899d20c91
Lajos Meszaros <lajos.meszaros@performgroup.com>
2016-02-29 16:39:00 +0100

    Moved comment on retuning to relevant ticket, added an example scala file

--- microtonal/monochord/js/app.js
+++ microtonal/monochord/js/app.js
@@ -13,6 +13,7 @@ angular
 		
 		// --------------
 		
+		/*
 		$scope.baseVolume = 30;
 		$scope.baseFrequency = 100;
 		$scope.sets = [];
@@ -27,89 +28,8 @@ angular
 			baseVolume : 'baseVolume',
 			retune : 'retune'
 		});
+		*/
 		
-		// --------------
 		
-		var step = 700;
-		
-		setTimeout(function(){
-			var setId = model.sets.add();
-			model.cents.add(setId, 0, 100, true);
-			model.cents.add(setId, step);
-			
-			model.commit();
-			setTimeout(function(){
-				var setId = model.sets.add();
-				model.cents.add(setId, 0, 100, true);
-				model.cents.add(setId, step);
-				model.commit();
-				setTimeout(function(){
-					var setId = model.sets.add();
-					model.cents.add(setId, 0, 100, true);
-					model.cents.add(setId, step);
-					model.commit();
-					setTimeout(function(){
-						var setId = model.sets.add();
-						model.cents.add(setId, 0, 100, true);
-						model.cents.add(setId, step);
-						model.commit();
-					}, 1000);
-				}, 1000);
-			}, 1000);
-		}, 0);
 	}])
 ;
-
-/*
-## About retuning
-
-base frequency:100Hz
-
-[Strings]
-
-set:{3, 4, 5}  // retune:off
---------------
-set:300Hz, 400Hz, 500Hz
-
-
-set:{3, 4, 5}  // retune:lowest to base frequency
---------------
-set:100Hz, 133.3Hz, 166.6Hz
-
-
-set:{3, 4, 5}  // retune:highest to base frequency
---------------
-set:60Hz, 80Hz, 100Hz
-
-
-set1:{3, 4, 5}  // retune:off
-set2:{2, 3}     // retune:lowest to previous' highest
---------------
-set1:300Hz, 400Hz, 500Hz
-set2:500Hz, 750Hz
-
-
-[Cents]
-
-set:{400¢, 700¢}  // retune:off; could be converted to ratio: {1.2599, 1.4983}
----------------
-set:125.99Hz, 149.83Hz
-
-
-set:{400¢, 700¢}  // retune:lowest to base frequency
----------------
-set:100Hz, 118.92Hz
-
-
-set:{400¢, 700¢}  // retune:highest to base frequency
----------------
-set:84.08Hz, 100Hz
-
-==================================
-
-String multipliers actually indicate ratios on their own:
-3, 4, 5 are actually three ratios, namingly 1:3, 1:4 and 1:5
-
-Cents' being ratios are more straightforward:
-400¢, 700¢ are actually 1:1.2599, 1:1.4983
-*/
