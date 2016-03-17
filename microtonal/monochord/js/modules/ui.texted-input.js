7e94b44468f79ac96ca01085a3dba022319b6155
Louis <m_lajos@hotmail.com>
2016-03-17 01:07:37 +0100

    Tweaked palette a bit more; turned button into a more general UI element; created buttongroup concept

--- microtonal/monochord/js/modules/ui.texted-input.js
+++ microtonal/monochord/js/modules/ui.texted-input.js
@@ -9,13 +9,25 @@ angular
 				label : '='
 			},
 			transclude: true,
-			template : '<form class="ui texted-input" autocomplete="off" novalidate="novalidate"><label><span class="left">{{label}}</span><span class="right editor" ng-transclude></span></label></form>',
+			template : '<form class="ui texted-input" autocomplete="off"><label class="buttongroup"><span class="button disabled left">{{label}}</span><span class="button disabled right editor" ng-transclude></span></label></form>',
 			link : function(scope, element, attrs){
-				element.find('input').on('focus', function(){
-					this.parentNode.classList.add('active');
-				}).on('blur', function(){
-					this.parentNode.classList.remove('active');
-				})
+				element.find('input')
+					/*
+					.on('focus', function(){
+						this.parentNode.classList.add('active');
+					})
+					.on('blur', function(){
+						this.parentNode.classList.remove('active');
+					})
+					*/
+					.on('input', function(){
+						if(this.classList.contains('ng-invalid')){
+							this.parentNode.classList.add('red');
+						}else{
+							this.parentNode.classList.remove('red');
+						}
+					})
+				;
 			}
 		};
 	})
