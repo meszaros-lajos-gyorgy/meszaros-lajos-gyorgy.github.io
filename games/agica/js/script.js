35aec124df34ad9a98011fa96f0cad5d8cd81705
Lajos Meszaros <lajos.meszaros@performgroup.com>
2016-11-02 17:28:36 +0100

    Achieved functionality of v0.2 and finished resource loading system

--- games/agica/js/script.js
+++ games/agica/js/script.js
@@ -1,73 +1,287 @@
 (function(){
 	'use strict';
-
-	var loadCounter = 0;
-	var pressed = 0;
-	var keyPressed = false;
-	var meow, normal, active;
-
-	function onPress(){
-		if(pressed === 0){
-			meow.play();
-			normal.className = 'hidden';
-			active.className = '';
-		}
-		pressed++;
+	
+	function isValidReadyState(){
+		return (document.readyState === 'interactive' || document.readyState === 'complete');
 	}
 	
-	function onRelease(){
-		pressed--;
-		if(pressed === 0){
-			meow.pause();
-			meow.currentTime = 0;
-			normal.className = '';
-			active.className = 'hidden';
+	function onReady(handler){
+		if(isValidReadyState()){
+			handler();
+		}else{
+			var rsHandler = function(){
+				document.removeEventListener('readystatechange', rsHandler);
+				if(isValidReadyState()){
+					handler();
+				}
+			};
+			document.addEventListener('readystatechange', rsHandler);
 		}
 	}
 	
-	function loaded(e){
-		loadCounter++;
-		if(loadCounter === 3){
-			document.body.addEventListener('keydown', function(e){
-				if(e.keyCode === 32 && !keyPressed){
-					keyPressed = true;
+	var load = {
+		_loads : {
+			image : {},
+			audio : {}
+		},
+		from : function(resources){
+			var self = this;
+			this._resources = resources;
+			return this;
+		},
+		to : function(){
+			return this._loads;
+		},
+		image : function(index){
+			var src = 'resources/image/' + (!isNaN(index) ? this._resources.image[index] : index);
+			var self = this;
+			return new Promise(function(resolve, reject){
+				var img = document.createElement('img');
+				img.addEventListener('load', function(){
+					self._loads.image[index] = this;
+					resolve(src);
+				});
+				img.addEventListener('error', function(e){
+					reject(new Error('Could not load image: "' + src + '"'));
+				});
+				img.src = src;
+			});
+		},
+		audio : function(index){
+			var src = 'resources/audio/' + (!isNaN(index) ? this._resources.audio[index] : index);
+			var self = this;
+			return new Promise(function(resolve, reject){
+				var audio = document.createElement('audio');
+				audio.addEventListener('canplaythrough', function(){
+					self._loads.audio[index] = this;
+					resolve(src);
+				});
+				audio.addEventListener('error', function(e){
+					reject(new Error('could not load audio: "' + src + '"'));
+				});
+				audio.src = src;
+				audio.controls = true;
+			});
+		}
+	};
+	
+	load.all = {
+		_source : function(type){
+			var sources = [];
+			Object.keys(load._resources[type]).forEach(function(index){
+				sources.push(load[type](index));
+			});
+			return Promise.all(sources);
+		},
+		image : function(){
+			return this._source('image');
+		},
+		audio : function(){
+			return this._source('audio');
+		}
+	};
+	
+	function setText(text){
+		document.querySelector('p').innerHTML = text;
+	}
+	
+	function bindControls(onPress, onRelease){
+		var keyPressed = false;
+		var pressed = 0;
+		
+		document.body.addEventListener('keydown', function(e){
+			if(e.keyCode === 32 && !keyPressed){
+				keyPressed = true;
+				if(pressed === 0){
 					onPress();
 				}
+				pressed++;
+			}
+		});
+		document.body.addEventListener('keyup', function(e){
+			if(e.keyCode === 32 && keyPressed){
+				keyPressed = false;
+				pressed--;
+				if(pressed === 0){
+					onRelease();
+				}
+			}
+		});
+		if(window.TouchEvent){
+			document.body.addEventListener('touchstart', function(e){
+				e.preventDefault();
+				if(pressed === 0){
+					onPress();
+				}
+				pressed++;
 			});
-			document.body.addEventListener('keyup', function(e){
-				if(e.keyCode === 32 && keyPressed){
-					keyPressed = false;
+			document.body.addEventListener('touchend', function(){
+				pressed--;
+				if(pressed === 0){
+					onRelease()
+				}
+			});
+		}else{
+			document.body.addEventListener('mousedown', function(e){
+				e.preventDefault();
+				if(pressed === 0){
+					onPress();
+				}
+				pressed++;
+			});
+			document.body.addEventListener('mouseup', function(){
+				pressed--;
+				if(pressed === 0){
 					onRelease();
 				}
 			});
-			if(window.TouchEvent){
-				document.body.addEventListener('touchstart', function(e){
-					e.preventDefault();
-					onPress();
-				});
-				document.body.addEventListener('touchend', onRelease);
-			}else{
-				document.body.addEventListener('mousedown', function(e){
-					e.preventDefault();
-					onPress();
-				});
-				document.body.addEventListener('mouseup', onRelease);
-			}
-			
-			normal.className = '';
 		}
 	}
-
-	document.addEventListener('readystatechange', function(){
-		if(document.readyState === 'interactive'){
-			meow = document.querySelector('audio');
-			meow.addEventListener('canplaythrough', loaded);
+	
+	function show(what){
+		what.classList.remove('hidden');
+	}
+	
+	function hide(what){
+		what.classList.add('hidden');
+	}
+	
+	// ------------------
+	
+	var SPACE = 32;
+	
+	var resources = {
+		image : [
+			'active.jpg',
+			'active-desat1.jpg',
+			'active-desat2.jpg',
+			'active-desat3.jpg',
+			'active-desat4.jpg',
+			'normal.jpg',
+			'normal-desat1.jpg',
+			'normal-desat2.jpg',
+			'normal-desat3.jpg',
+			'normal-desat4.jpg'
+		],
+		audio : [
+			'meow.mp3',
+			'meow-dark.mp3',
+			'tension-loop.mp3',
+			'heartbeat-normal.mp3',
+			'heartbeat-faster.mp3',
+			'heartbeat-fast.mp3'
+		]
+	};
+	
+	var texts = {
+		loading : 'Loading resources, please wait...',
+		welcome : 'Press and hold <code>space</code> or hold down the mouse button/touch screen anywhere!'
+	};
+	
+	// ------------------
+	
+	var finishResolve;
+	var finishReject;
+	var finished = new Promise(function(resolve, reject){
+		finishResolve = resolve;
+		finishReject = reject;
+	});
+	
+	var finishedImage = !(resources.image && resources.image.length);
+	var finishedAudio = !(resources.audio && resources.audio.length);
+	
+	load.from(resources);
+	
+	load.all.image().then(function(){
+		onReady(function(){
+			var elements = load.to();
+			Object.values(elements.image).forEach(function(imgTag){
+				document.querySelector('body>div').appendChild(imgTag);
+				imgTag.classList.add('hidden');
+			});
 			
-			normal = document.querySelector('img.normal');
-			normal.addEventListener('load', loaded);
+			finishedImage = true;
+			if(finishedImage && finishedAudio){
+				finishResolve();
+			}
+		});
+	}, finishReject);
+	
+	load.all.audio().then(function(){
+		onReady(function(){
+			var elements = load.to();
+			Object.values(elements.audio).forEach(function(audioTag){
+				document.body.appendChild(audioTag);
+				audioTag.classList.add('hidden');
+			});
 			
-			active = document.querySelector('img.active');
-			active.addEventListener('load', loaded);
-		}
-	})
+			finishedAudio = true;
+			if(finishedImage && finishedAudio){
+				finishResolve();
+			}
+		});
+	}, finishReject);
+	
+	onReady(function(){
+		setText(texts.loading);
+	});
+	
+	finished.then(function(){
+		var stage = {
+			face: 0,
+			heartbeat: 0
+		};
+		
+		var normal = [
+			document.querySelector('img[src$="normal.jpg"]'),
+			document.querySelector('img[src$="normal-desat1.jpg"]'),
+			document.querySelector('img[src$="normal-desat2.jpg"]'),
+			document.querySelector('img[src$="normal-desat3.jpg"]'),
+			document.querySelector('img[src$="normal-desat4.jpg"]')
+		];
+		var active = [
+			document.querySelector('img[src$="active.jpg"]'),
+			document.querySelector('img[src$="active-desat1.jpg"]'),
+			document.querySelector('img[src$="active-desat2.jpg"]'),
+			document.querySelector('img[src$="active-desat3.jpg"]'),
+			document.querySelector('img[src$="active-desat4.jpg"]')
+		];
+		
+		var meow = document.querySelector('audio[src$="meow.mp3"]');
+		var growl = document.querySelector('audio[src$="meow-dark.mp3"]');
+		var tention = document.querySelector('audio[src$="tension-loop.mp3"]');
+		var heartbeat = [
+			document.querySelector('audio[src$="heartbeat-normal.mp3"]'),
+			document.querySelector('audio[src$="heartbeat-faster.mp3"]'),
+			document.querySelector('audio[src$="heartbeat-fast.mp3"]')
+		];
+		heartbeat.concat(tention).forEach(function(audio){
+			audio.loop = true;
+		});
+		
+		// --------
+		
+		setText(texts.welcome);
+		show(normal[stage.face]);
+		
+		// heartbeat[stage.heartbeat].play();
+		// tention.play();
+		
+		bindControls(function(){
+			if(stage.face < 4){
+				meow.play();
+				hide(normal[stage.face]);
+				show(active[stage.face]);
+			}
+		}, function(){
+			if(stage.face < 4){
+				meow.pause();
+				meow.currentTime = 0;
+				hide(active[stage.face]);
+				show(normal[stage.face]);
+			}
+		});
+	}, function(e){
+		console.error(e);
+	});
 })();
