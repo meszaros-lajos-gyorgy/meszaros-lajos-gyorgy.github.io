12b7e4ba39d5f7f892c2e175f9cb81df0de5662f
Lajos Meszaros <m_lajos@hotmail.com>
2023-07-06 15:43:03 +0200

    feat(Slider): scaffolding

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+// TODO: https://css-tricks.com/styling-cross-browser-compatible-range-inputs-css/
+
+type SliderProps = {}
+
+export const Slider: FC<SliderProps> = () => {
+  return null
+}
