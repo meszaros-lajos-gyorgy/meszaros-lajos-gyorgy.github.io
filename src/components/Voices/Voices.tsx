6a0dc6e3c60d02df023b8eb00c404eaf41225b71
Lajos Meszaros <m_lajos@hotmail.com>
2024-02-10 02:32:36 +0100

    refactor(Voices): utilize times to render each Voice

--- src/components/Voices/Voices.tsx
+++ src/components/Voices/Voices.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { Voice } from '@components/Voice/Voice'
 import { NUMBER_OF_VOICES } from '@src/constants'
+import { times } from '@src/functions'
 import s from './Voices.module.scss'
 
 type VoicesProps = {}
@@ -9,12 +10,9 @@ export const Voices: FC<VoicesProps> = () => {
   return (
     <section className={s.Voices}>
       <h2>Voices</h2>
-      {'x'
-        .repeat(NUMBER_OF_VOICES)
-        .split('')
-        .map((_, idx) => {
-          return <Voice key={idx} idx={idx} />
-        })}
+      {times((idx) => {
+        return <Voice key={idx} idx={idx} />
+      }, NUMBER_OF_VOICES)}
     </section>
   )
 }
