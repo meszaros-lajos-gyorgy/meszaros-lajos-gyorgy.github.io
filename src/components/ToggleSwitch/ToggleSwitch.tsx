5c72901ebc690f50175eb29b0f1a2ff1dc2e65b1
Lajos Meszaros <m_lajos@hotmail.com>
2024-02-23 15:53:37 +0100

    feat(ToggleSwitch): allow any return value of the onClick handler

--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -4,7 +4,7 @@ import s from './ToggleSwitch.module.scss'
 
 type ToggleSwitchProps = {
   isOn: boolean
-  onClick: () => void | Promise<void>
+  onClick: () => any
 }
 
 export const ToggleSwitch: FC<ToggleSwitchProps> = ({ isOn, onClick }) => {
