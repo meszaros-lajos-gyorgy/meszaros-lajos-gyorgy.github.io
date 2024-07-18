f83fbcb2bc07ba8df91d60deaeac45bdc8964a92
Lajos Meszaros <m_lajos@hotmail.com>
2024-07-18 12:49:32 +0200

    feat(html): de-reactify header and move it to the markup + add footer

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react'
 import { Provider } from 'react-redux'
-import { Header } from '@components/Header/Header'
 import { Voices } from '@components/Voices/Voices'
 import { store } from '@src/store/store'
 
@@ -9,7 +8,6 @@ type AppProps = {}
 export const App: FC<AppProps> = () => {
   return (
     <Provider store={store}>
-      <Header />
       <Voices />
     </Provider>
   )
